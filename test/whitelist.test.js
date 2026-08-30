9a0ec72374a487ef096a9aa8e392c475829bc146
duyxyz <duyxyz.git@gmail.com>
2026-08-30 15:07:55 +0700

    feat: implement global constants and add unit tests for whitelist service logic

--- test/whitelist.test.js
+++ test/whitelist.test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 // Mock globalThis.ScrollHideConstants
 global.globalThis = global;
 global.ScrollHideConstants = {
-    RESTRICTED_HOSTS: ['chromewebstore.google.com'],
+    RESTRICTED_HOSTS: [],
     RESTRICTED_PROTOCOLS: ['chrome:', 'edge:', 'about:', 'chrome-extension:']
 };
 
@@ -35,7 +35,7 @@ function runTests() {
     // Test isRestrictedUrl
     assert.strictEqual(service.isRestrictedUrl('chrome://settings'), true);
     assert.strictEqual(service.isRestrictedUrl('https://wikipedia.org'), false);
-    assert.strictEqual(service.isRestrictedUrl('https://chromewebstore.google.com/detail/123'), true);
+    assert.strictEqual(service.isRestrictedUrl('https://chromewebstore.google.com/detail/123'), false);
     assert.strictEqual(service.isRestrictedUrl('https://chrome.google.com/'), false);
     console.log('✅ isRestrictedUrl: OK');
 
